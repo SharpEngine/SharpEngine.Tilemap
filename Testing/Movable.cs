383a7419ee611683fe38671695fb935fec5d9174
Alexis Huvier <huvier.alexis@gmail.com>
2024-12-27 17:11:58 +0100

    feat(testing): update

---
+++ Testing/Movable.cs
@@ -0,0 +1,21 @@
+﻿using SharpEngine.Core.Component;
+using SharpEngine.Core.Entity;
+using SharpEngine.Core.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class Movable: Entity
+    {
+        public Movable()
+        {
+            AddComponent(new TransformComponent(Vec2.Zero));
+            AddComponent(new ControlComponent());
+            AddComponent(new SpriteComponent("tile1"));
+        }
+    }
+}
--- Testing/MyScene.cs
+++ Testing/MyScene.cs
@@ -9,6 +9,9 @@ public class MyScene: Scene
 {
     public MyScene()
     {
+        var m1 = new Movable();
+        AddEntity(m1);
+
         var entity = new Entity();
         entity.AddComponent(new TransformComponent(new SharpEngine.Core.Math.Vec2(200), new SharpEngine.Core.Math.Vec2(4)));
         entity.AddComponent(new TilemapComponent("Resource/tilemap.json"));
